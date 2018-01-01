2bf6971b195e8bf3ca561dff76d1a54e47e15dd0
Carlos Brito <carlosdotb@gmail.com>
2017-12-31 19:26:25 -0600

    Finish basic functionality.
    Next steps are:
    - Implement move constructor
    - Implement constructor from container
    - Wrap all constructors, although this may be left just as a forwarding
      constructor rather than to wrap all overloads
    - Test thread safety
    - Test the class

--- object_pool.hpp
+++ object_pool.hpp
@@ -29,17 +29,49 @@
 namespace carlosb
 {
     namespace internal
-    {
+    {   
+        template <class...>
+        using void_t = void;
+
+        template <class T, class = void>
+        struct is_basic_lockable : public std::false_type
+        {};
+
+        template <class T>
+        struct is_basic_lockable<T, void_t<
+            decltype(std::declval<T>().lock()),
+            decltype(std::declval<T>().unlock())
+        >> : std::true_type
+        {};
+
+        template <class T, class = void>
+        struct is_allocator : public std::false_type
+        {};
+
+        template <class T>
+        struct is_allocator<T, void_t<
+            typename T::value_type,
+            typename T::size_type,
+            typename std::is_same<
+                decltype(std::declval<T>().allocate(std::declval<typename T::size_type>())),
+                typename T::value_type*>::type,
+            decltype(std::declval<T>().deallocate(std::declval<typename T::value_type*>(), std::declval<typename T::size_type>()))
+        >> : std::true_type
+        {};
     }
 
     template <
         class T,
-        class Allocator         = std::allocator<T>,
-        class RecursiveMutex    = std::recursive_mutex,
-        class LockGuard         = std::lock_guard<RecursiveMutex>
+        class Allocator     = std::allocator<T>,
+        class Mutex         = std::mutex,
+        class LockGuard     = std::lock_guard<Mutex>
     >
     class object_pool
     {
+        static_assert(internal::is_allocator<Allocator>::value,
+                      "Allocator template parameter must satisfy the BasicAllocator requirement.");
+        static_assert(internal::is_basic_lockable<Mutex>::value,
+                      "Mutex template parameter must satisfy the BasicLockable requirement.");
     public:
         // --- HELPER CLASSES ---------------------
         class impl;
@@ -51,47 +83,62 @@ namespace carlosb
         using lv_reference      = T&;
         using rv_reference      = T&&;
         using const_reference   = const T&;
+        
         using acquired_type     = std::unique_ptr<T, deleter>;
+        using stack_type        = std::stack<T*>;
 
         using size_type         = std::size_t;
 
         using allocator_type    = Allocator;
-        using mutex_type        = RecursiveMutex;
+        using mutex_type        = Mutex;
         using lock_guard        = LockGuard;
 
-        explicit object_pool(size_type count, const T& value, const Allocator& alloc = Allocator())
-            : m_pool(std::make_shared<impl>(count, value, alloc))
+        template <class... Args>
+        object_pool(Args&&... args)
+            :   m_pool(std::make_shared<impl>(std::forward<Args>(args)...))
         {}
 
         acquired_type acquire()
         {
+            lock_guard lock(m_modifier_mutex);
             return m_pool->acquire();
         }
 
         void insert(const_reference value)
         {
+            lock_guard lock(m_modifier_mutex);
             m_pool->insert(value);
         }
 
         void insert(rv_reference value)
         {
+            lock_guard lock(m_modifier_mutex);
             m_pool->insert(std::move(value));
         }
 
         template <class... Args>
         void emplace(Args&&... args)
         {
+            lock_guard lock(m_modifier_mutex);
             m_pool->emplace(std::forward<Args>(args)...);
         }
 
         void resize(size_type count)
         {
+            lock_guard lock(m_modifier_mutex);
             m_pool->resize(count);
         }
 
         void resize(size_type count, const value_type& value)
         {
+            lock_guard lock(m_modifier_mutex);
             m_pool->resize(count, value);
+        }
+
+        void reserve(size_type count)
+        {
+            lock_guard lock(m_modifier_mutex);
+            m_pool->reserve(count);
         }
 
         size_type size() const
@@ -110,10 +157,11 @@ namespace carlosb
         }
 
     private:
-        std::shared_ptr<impl> m_pool;
+        std::shared_ptr<impl>       m_pool;
+        mutex_type                  m_modifier_mutex;     ///< Controls access to the modifiers.
     };
 
-    // --- IMPL IMPLEMENTATIONS -------------------------------------
+    // --- impl IMPLEMENTATION --------------------------------------
     template <
         class T,
         class Allocator,
@@ -122,13 +170,31 @@ namespace carlosb
     >
     class object_pool<T, Allocator, Mutex, LockGuard>::impl : public std::enable_shared_from_this<impl>
     {
+        // --- Friend Functions ----------------
         friend void deleter::operator()(T* ptr);
+
     public:
+        template <typename = typename std::enable_if<
+            std::is_default_constructible<Allocator>::value>
+        ::type
+        >
+        impl()
+            : impl(Allocator())
+        {}
+
+        explicit impl(const Allocator& alloc)
+            :   m_size(0),
+                m_capacity(4),
+                m_allocator(alloc)
+        {
+            m_pool = m_allocator.allocate(m_capacity);
+        }
+
         template <typename = typename std::enable_if<
             std::is_default_constructible<T>::value && std::is_copy_constructible<T>::value>
         ::type
         >
-        explicit impl(size_type count, const T& value, const Allocator& alloc = Allocator())
+        impl(size_type count, const T& value, const Allocator& alloc = Allocator())
             :   m_size(count),
                 m_capacity(count),
                 m_allocator(alloc)
@@ -141,13 +207,14 @@ namespace carlosb
                 this->insert(m_pool + i);
         }
 
-        explicit impl(const Allocator& alloc = Allocator())
-            :   m_size(0),
-                m_capacity(4),
+        explicit impl(size_type count, const Allocator& alloc = Allocator())
+            :   m_size(count),
+                m_capacity(count),
                 m_allocator(alloc)
         {
-            m_pool = m_allocator.allocate(m_capacity);
+            m_pool = m_allocator.allocate(count);
         }
+
 
         ~impl()
         {
@@ -156,97 +223,51 @@ namespace carlosb
 
         void reserve(size_type n)
         {
-            lock_guard lock(m_pool_mutex);
-
-            if (m_pool)
-                m_allocator.deallocate(m_pool, m_capacity);
-
+            m_allocator.deallocate(m_pool, m_capacity);
             m_pool = m_allocator.allocate(n);
             m_capacity = n;
         }
 
         acquired_type acquire()
         {
+            if (m_free.empty())
             {
-                lock_guard lock(m_stack_mutex);
-
-                if (m_free.empty())
-                {
-                    throw std::out_of_range("No free objects available.");
-                }
-                else
-                {   
-                    auto obj = acquired_type(m_free.top(), deleter{impl::shared_from_this()});
-                    m_free.pop();
-                    return std::move(obj);
-                }  
-            }   
+                throw std::out_of_range("No free objects available.");
+            }
+            else
+            {   
+                auto obj = acquired_type(m_free.top(), deleter{impl::shared_from_this()});
+                m_free.pop();
+                return std::move(obj);
+            }  
         }
 
         void insert(const_reference value)
         {
-            {
-                lock_guard lock_pool(m_pool_mutex);
-                if (m_size == m_capacity)
-                    this->reallocate(m_capacity * 2);
-
-                m_pool[m_size] = value;
-            }
-
-            {
-                lock_guard lock_stack(m_stack_mutex);
-                m_free.push(m_pool + m_size);
-            }
-
-            {
-                lock_guard lock_pool(m_pool_mutex);
-                ++m_size;
-            }
+            if (m_size == m_capacity)
+                this->reallocate(m_capacity * 2);
+            m_pool[m_size] = value;
+            m_free.push(m_pool + m_size);
+            ++m_size;
         }
 
         void insert(rv_reference value)
         {
-            {
-                lock_guard lock_pool(m_pool_mutex);
-                if (m_size == m_capacity)
-                    this->reallocate(m_capacity * 2);
-
-                m_pool[m_size] = std::move(value);
-            }
-
-            {
-                lock_guard lock_stack(m_stack_mutex);
-                m_free.push(m_pool + m_size);
-            }
-            
-            {
-                lock_guard lock_pool(m_pool_mutex);
-                ++m_size;
-            }
-            
+            if (m_size == m_capacity)
+                this->reallocate(m_capacity * 2);
+            m_pool[m_size] = std::move(value);
+            m_free.push(m_pool + m_size);
+            ++m_size;
         }
 
         template <class... Args>
         void emplace(Args&&... args)
         {
-            {
-                lock_guard lock_pool(m_pool_mutex);
-
-                if (m_size == m_capacity)
-                    this->reallocate(m_capacity * 2);
-
-                m_pool[m_size] = T(std::forward<Args>(args)...);
-            }
-
-            {
-                lock_guard lock_stack(m_stack_mutex);
-                m_free.push(m_pool + m_size); 
-            }
-
-            {
-                lock_guard lock_pool(m_pool_mutex);
-                ++m_size;
-            }   
+            if (m_size == m_capacity)
+                this->reallocate(m_capacity * 2);
+            m_pool[m_size] = T(std::forward<Args>(args)...);
+            m_free.push(m_pool + m_size); 
+            ++m_size;
         }
 
         void resize(size_type count)
@@ -300,7 +321,6 @@ namespace carlosb
     private:
         void insert(T* obj)
         {
-            lock_guard lock_stack(m_stack_mutex);
             m_free.push(obj);
         }
 
@@ -308,7 +328,6 @@ namespace carlosb
         {
             T* new_pool = m_allocator.allocate(count);
             {
-                lock_guard lock(m_pool_mutex);
                 std::memcpy(new_pool, m_pool, m_size * sizeof(T));
                 m_allocator.deallocate(m_pool, m_capacity);
                 m_pool = new_pool;
@@ -317,15 +336,13 @@ namespace carlosb
         }
 
         T*                                          m_pool;
+        stack_type                                  m_free;
         allocator_type                              m_allocator;
         size_type                                   m_size;
         size_type                                   m_capacity;
-        std::stack<T*>                              m_free;
-        mutex_type                                  m_pool_mutex;       ///< Controls access to the memory.
-        mutex_type                                  m_stack_mutex;      ///< Controls access to the stack of free objects.
     };
 
-    // --- deleter IMPLEMENTATIONS ---------------------------
+    // --- deleter IMPLEMENTATION -----------------------------------
     template <
         class T,
         class Allocator,
